78d5cd6f2d828ffeae7eccac11ba723d0f031200
Grant Cloyd <dgcloyd@gmail.com>
2023-11-20 18:41:44 -0500

    add delete, refactor, add tailwind css

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,13 +12,37 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
   end
 
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.update!(topics_params)
+
+    redirect_to topic_path(@topic), notice: "Topic updated!"
+
+  rescue ActiveRecord::RecordNotFound
+    redirect_to topics_path, notice: "This topic no longer exists"
+  end
+
   def create
-    Topic.create!(topics_params)
+    @topic = Topic.create!(topics_params)
     
-    redirect_to request.referer, notice: "Successfuly created"
+    redirect_to topic_path(@topic), notice: "Successfuly created!"
 
     rescue ArgumentError => error
       redirect_to request.referer, notice: "Not Sucessfully created. Error: #{error.message}"
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy!
+
+    redirect_to topics_path, notice: "Topic deleted!"
+
+  rescue ActiveRecord::RecordNotFound
+    redirect_to topics_path, notice: "This topic no longer exists"
   end
 
   def topics_params
