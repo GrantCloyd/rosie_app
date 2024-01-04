5ff57873a6b30588cf0b6543471e42da7fc4e650
Grant Cloyd <dgcloyd@gmail.com>
2024-01-04 13:49:16 -0500

    okay - metaprogramming is cute but this is cleaner- counter refactor

--- app/controllers/groups/sections/posts_controller.rb
+++ app/controllers/groups/sections/posts_controller.rb
@@ -10,8 +10,7 @@ module Groups
       def show
         @post = Post.includes(:comments, :user_reactions).find(params[:id])
         @current_user_reaction = @post.user_reactions.select { |p| p.user_id == current_user.id }&.first
-        @reaction_counter = UserReactions::CountPresenterService.new(@post.user_reactions)
-        @reaction_counter.count
+        @reaction_counter = UserReactions::CountPresenterService.new(@post.user_reactions).count
         @comments = @post.comments
 
         respond_to do |format|
--- app/services/user_reactions/count_presenter_service.rb
+++ app/services/user_reactions/count_presenter_service.rb
@@ -3,24 +3,16 @@
 # the intent is to reduce overall db hits
 module UserReactions
   class CountPresenterService
-
     def initialize(user_reactions)
       @user_reactions = user_reactions
-      # set a count dynamically to 0 and dynamically create readers
-      UserReaction.statuses.each_key do |s| 
-        pluralized_name = s.pluralize
-        instance_variable_set("@#{pluralized_name}", 0)
-        define_singleton_method("#{pluralized_name}") {
-          instance_variable_get("@#{pluralized_name}")
-        }
-      end
+      @counter = Hash.new(0)
     end
 
     def count
-      @user_reactions.each do |ur| 
-        reaction_count = instance_variable_get("@#{ur.status.pluralize}")
-        instance_variable_set("@#{ur.status.pluralize}", (reaction_count + 1))
+      @user_reactions.each do |ur|
+        @counter[ur.status] += 1
       end
+      @counter.transform_keys { |k| k.pluralize.titleize }
     end
   end
 end
