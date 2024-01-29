ef9f99f7e487a9d1c7d8dd67cd8731c5ff59f73f
Grant Cloyd <dgcloyd@gmail.com>
2024-01-29 10:10:44 -0500

    add migration

---
+++ app/models/token.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: tokens
+#
+#  id         :bigint           not null, primary key
+#  code       :string           not null
+#  expires_at :datetime         not null
+#  kind       :integer          not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_tokens_on_code              (code) USING hash
+#  index_tokens_on_expires_at        (expires_at)
+#  index_tokens_on_kind_and_user_id  (kind,user_id) UNIQUE
+#  index_tokens_on_user_id           (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Token < ApplicationRecord
+  belongs_to :user
+
+  enum kind: {
+    authentication: 0,
+    recovery: 1
+  }
+end
